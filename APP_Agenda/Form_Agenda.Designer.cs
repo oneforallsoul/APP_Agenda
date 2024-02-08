45fb7d4a76d5eae94e063ed3ae623dd0d6af25d3
oneforallsoul <nicolasmellosilva12@gmail.com>
2024-02-08 17:12:12 -0300

    shut up

---
+++ APP_Agenda/Form_Agenda.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace APP_Agenda
+{
+    partial class Form_Agenda
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Agenda));
+            this.textBoxNOME = new System.Windows.Forms.TextBox();
+            this.labelNOME = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSOBRENOME = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxTELEFONE = new System.Windows.Forms.TextBox();
+            this.labelCELULAR = new System.Windows.Forms.Label();
+            this.textBoxCELULAR = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxEMAIL = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxENDERECO = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxREDESOCIAL = new System.Windows.Forms.TextBox();
+            this.buttonSALVAR = new System.Windows.Forms.Button();
+            this.buttonALTERAR = new System.Windows.Forms.Button();
+            this.buttonAPAGAR = new System.Windows.Forms.Button();
+            this.dataGridViewAGENDA = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxPESQUISA = new System.Windows.Forms.TextBox();
+            this.buttonBUSCA = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAGENDA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxNOME
+            // 
+            this.textBoxNOME.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNOME.Location = new System.Drawing.Point(90, 36);
+            this.textBoxNOME.Name = "textBoxNOME";
+            this.textBoxNOME.Size = new System.Drawing.Size(250, 23);
+            this.textBoxNOME.TabIndex = 0;
+            // 
+            // labelNOME
+            // 
+            this.labelNOME.AutoSize = true;
+            this.labelNOME.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNOME.Location = new System.Drawing.Point(39, 39);
+            this.labelNOME.Name = "labelNOME";
+            this.labelNOME.Size = new System.Drawing.Size(45, 17);
+            this.labelNOME.TabIndex = 1;
+            this.labelNOME.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sobrenome";
+            // 
+            // textBoxSOBRENOME
+            // 
+            this.textBoxSOBRENOME.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSOBRENOME.Location = new System.Drawing.Point(126, 65);
+            this.textBoxSOBRENOME.Name = "textBoxSOBRENOME";
+            this.textBoxSOBRENOME.Size = new System.Drawing.Size(214, 23);
+            this.textBoxSOBRENOME.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Telefone";
+            // 
+            // textBoxTELEFONE
+            // 
+            this.textBoxTELEFONE.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTELEFONE.Location = new System.Drawing.Point(109, 94);
+            this.textBoxTELEFONE.Name = "textBoxTELEFONE";
+            this.textBoxTELEFONE.Size = new System.Drawing.Size(231, 23);
+            this.textBoxTELEFONE.TabIndex = 4;
+            // 
+            // labelCELULAR
+            // 
+            this.labelCELULAR.AutoSize = true;
+            this.labelCELULAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCELULAR.Location = new System.Drawing.Point(39, 126);
+            this.labelCELULAR.Name = "labelCELULAR";
+            this.labelCELULAR.Size = new System.Drawing.Size(52, 17);
+            this.labelCELULAR.TabIndex = 7;
+            this.labelCELULAR.Text = "Celular";
+            // 
+            // textBoxCELULAR
+            // 
+            this.textBoxCELULAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCELULAR.Location = new System.Drawing.Point(109, 123);
+            this.textBoxCELULAR.Name = "textBoxCELULAR";
+            this.textBoxCELULAR.Size = new System.Drawing.Size(231, 23);
+            this.textBoxCELULAR.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(39, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "E-mail";
+            // 
+            // textBoxEMAIL
+            // 
+            this.textBoxEMAIL.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEMAIL.Location = new System.Drawing.Point(109, 152);
+            this.textBoxEMAIL.Name = "textBoxEMAIL";
+            this.textBoxEMAIL.Size = new System.Drawing.Size(231, 23);
+            this.textBoxEMAIL.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(39, 237);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Endereço";
+            // 
+            // textBoxENDERECO
+            // 
+            this.textBoxENDERECO.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxENDERECO.Location = new System.Drawing.Point(42, 257);
+            this.textBoxENDERECO.Multiline = true;
+            this.textBoxENDERECO.Name = "textBoxENDERECO";
+            this.textBoxENDERECO.Size = new System.Drawing.Size(298, 72);
+            this.textBoxENDERECO.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(39, 184);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Rede Social";
+            // 
+            // textBoxREDESOCIAL
+            // 
+            this.textBoxREDESOCIAL.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxREDESOCIAL.Location = new System.Drawing.Point(42, 204);
+            this.textBoxREDESOCIAL.Name = "textBoxREDESOCIAL";
+            this.textBoxREDESOCIAL.Size = new System.Drawing.Size(298, 23);
+            this.textBoxREDESOCIAL.TabIndex = 10;
+            // 
+            // buttonSALVAR
+            // 
+            this.buttonSALVAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSALVAR.Image = global::APP_Agenda.Properties.Resources.btn_salvar;
+            this.buttonSALVAR.Location = new System.Drawing.Point(42, 369);
+            this.buttonSALVAR.Name = "buttonSALVAR";
+            this.buttonSALVAR.Size = new System.Drawing.Size(97, 29);
+            this.buttonSALVAR.TabIndex = 14;
+            this.buttonSALVAR.Text = "Salvar";
+            this.buttonSALVAR.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonSALVAR.UseVisualStyleBackColor = true;
+            this.buttonSALVAR.Click += new System.EventHandler(this.buttonSALVAR_Click);
+            // 
+            // buttonALTERAR
+            // 
+            this.buttonALTERAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonALTERAR.Image = global::APP_Agenda.Properties.Resources.btn_alterar;
+            this.buttonALTERAR.Location = new System.Drawing.Point(144, 369);
+            this.buttonALTERAR.Name = "buttonALTERAR";
+            this.buttonALTERAR.Size = new System.Drawing.Size(97, 29);
+            this.buttonALTERAR.TabIndex = 15;
+            this.buttonALTERAR.Text = "Alterar";
+            this.buttonALTERAR.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonALTERAR.UseVisualStyleBackColor = true;
+            this.buttonALTERAR.Click += new System.EventHandler(this.buttonALTERAR_Click);
+            // 
+            // buttonAPAGAR
+            // 
+            this.buttonAPAGAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAPAGAR.Image = global::APP_Agenda.Properties.Resources.btn_apagar;
+            this.buttonAPAGAR.Location = new System.Drawing.Point(245, 369);
+            this.buttonAPAGAR.Name = "buttonAPAGAR";
+            this.buttonAPAGAR.Size = new System.Drawing.Size(95, 29);
+            this.buttonAPAGAR.TabIndex = 16;
+            this.buttonAPAGAR.Text = "Apagar";
+            this.buttonAPAGAR.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonAPAGAR.UseVisualStyleBackColor = true;
+            this.buttonAPAGAR.Click += new System.EventHandler(this.buttonAPAGAR_Click);
+            // 
+            // dataGridViewAGENDA
+            // 
+            this.dataGridViewAGENDA.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAGENDA.Location = new System.Drawing.Point(360, 94);
+            this.dataGridViewAGENDA.Name = "dataGridViewAGENDA";
+            this.dataGridViewAGENDA.Size = new System.Drawing.Size(343, 304);
+            this.dataGridViewAGENDA.TabIndex = 17;
+            this.dataGridViewAGENDA.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridViewPESQUISA_MouseDoubleClick);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(359, 39);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(145, 17);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Pesquisar por NOME:";
+            // 
+            // textBoxPESQUISA
+            // 
+            this.textBoxPESQUISA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPESQUISA.Location = new System.Drawing.Point(360, 65);
+            this.textBoxPESQUISA.Name = "textBoxPESQUISA";
+            this.textBoxPESQUISA.Size = new System.Drawing.Size(279, 23);
+            this.textBoxPESQUISA.TabIndex = 18;
+            // 
+            // buttonBUSCA
+            // 
+            this.buttonBUSCA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBUSCA.Location = new System.Drawing.Point(645, 62);
+            this.buttonBUSCA.Name = "buttonBUSCA";
+            this.buttonBUSCA.Size = new System.Drawing.Size(58, 29);
+            this.buttonBUSCA.TabIndex = 22;
+            this.buttonBUSCA.Text = "Buscar";
+            this.buttonBUSCA.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonBUSCA.UseVisualStyleBackColor = true;
+            this.buttonBUSCA.Click += new System.EventHandler(this.buttonBUSCA_Click);
+            // 
+            // Form_Agenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 410);
+            this.Controls.Add(this.buttonBUSCA);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxPESQUISA);
+            this.Controls.Add(this.dataGridViewAGENDA);
+            this.Controls.Add(this.buttonAPAGAR);
+            this.Controls.Add(this.buttonALTERAR);
+            this.Controls.Add(this.buttonSALVAR);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxREDESOCIAL);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxENDERECO);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxEMAIL);
+            this.Controls.Add(this.labelCELULAR);
+            this.Controls.Add(this.textBoxCELULAR);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxTELEFONE);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSOBRENOME);
+            this.Controls.Add(this.labelNOME);
+            this.Controls.Add(this.textBoxNOME);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form_Agenda";
+            this.Text = "Agenda Pessoal";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAGENDA)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNOME;
+        private System.Windows.Forms.Label labelNOME;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSOBRENOME;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxTELEFONE;
+        private System.Windows.Forms.Label labelCELULAR;
+        private System.Windows.Forms.TextBox textBoxCELULAR;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxEMAIL;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxENDERECO;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxREDESOCIAL;
+        private System.Windows.Forms.Button buttonSALVAR;
+        private System.Windows.Forms.Button buttonALTERAR;
+        private System.Windows.Forms.Button buttonAPAGAR;
+        private System.Windows.Forms.DataGridView dataGridViewAGENDA;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxPESQUISA;
+        private System.Windows.Forms.Button buttonBUSCA;
+    }
+}
+
---
+++ APP_Agenda/Form_Agenda.cs
@@ -0,0 +1,244 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APP_Agenda
+{
+    public partial class Form_Agenda : Form
+    {
+        // Variaveis Publicas para o MySQL
+        string servidor;
+        MySqlConnection conexao;
+        MySqlCommand comando;
+
+        // Variaveis Publicas de Sistema
+        string idREGISTRO;
+
+        public Form_Agenda()
+        {
+            InitializeComponent();
+
+            // Utilização das variaveis publicas para o MySQL
+            servidor = "Server=localhost;Database=bd_agenda;Uid=root;Pwd=";
+            conexao = new MySqlConnection(servidor);
+            comando = conexao.CreateCommand();
+
+            // Preenche o DataGridViewAGENDA
+            ATUALIZA_Agenda();
+        }
+
+        private void LIMPAR_Formulario()
+        {
+            // Limpar Campos...
+            textBoxNOME.Clear();
+            textBoxSOBRENOME.Clear();
+            textBoxTELEFONE.Clear();
+            textBoxCELULAR.Clear();
+            textBoxEMAIL.Clear();
+            textBoxENDERECO.Clear();
+            textBoxREDESOCIAL.Clear();
+        }
+
+        private void ATUALIZA_Agenda()
+        {
+            try
+            {
+                conexao.Open();
+
+                comando.CommandText = "SELECT * FROM tbl_contatos;";
+                MySqlDataAdapter adaptadorAGENDA = new MySqlDataAdapter(comando);
+                DataTable tabelaAGENDA = new DataTable();
+                adaptadorAGENDA.Fill(tabelaAGENDA);
+
+                dataGridViewAGENDA.DataSource = tabelaAGENDA;
+                dataGridViewAGENDA.Columns["id"].HeaderText = "Código";
+            }
+            catch (Exception erro_mysql)
+            {
+                MessageBox.Show(erro_mysql.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        private void buttonSALVAR_Click(object sender, EventArgs e)
+        {
+            labelNOME.ForeColor = Color.Black;
+            labelCELULAR.ForeColor = Color.Black;
+
+            try
+            {
+                if (textBoxNOME.Text != "" && textBoxCELULAR.Text != "")
+                {
+                    conexao.Open();
+                    comando.CommandText = "INSERT INTO tbl_contatos(nome, sobrenome, telefone, celular, email, endereco, rede_social) VALUES ('" + textBoxNOME.Text + "', '" + textBoxSOBRENOME.Text + "', '" + textBoxTELEFONE.Text + "', '" + textBoxCELULAR.Text + "', '" + textBoxEMAIL.Text + "', '" + textBoxENDERECO.Text + "', '" + textBoxREDESOCIAL.Text + "');";
+                    comando.ExecuteNonQuery();
+                    MessageBox.Show("Contato adicionado com sucesso!");
+
+                    LIMPAR_Formulario();
+                }
+                else
+                {
+                    MessageBox.Show("Nome e/ou Celular estão em BRANCO! Por favor preencha!");
+
+                    if (textBoxNOME.Text == "")
+                    {
+                        textBoxNOME.Focus();
+                        labelNOME.ForeColor = Color.Red;                        
+                    }
+                    else
+                    {
+                        textBoxCELULAR.Focus();
+                        labelCELULAR.ForeColor = Color.Red;
+                    }
+                }
+            }
+            catch (Exception erro_mysql)
+            {
+                // Mensagem de erro - MySQL
+                // MessageBox.Show(erro_mysql.Message);
+
+                // Mensagem de erro - USUÁRIO
+                MessageBox.Show("Erro de Sistema. Solicite ajuda!");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            ATUALIZA_Agenda();
+
+        }
+
+        private void buttonAPAGAR_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente EXCLUIR este registro?", "Atenção!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try
+                {
+                    conexao.Open();
+
+                    comando.CommandText = "DELETE FROM tbl_contatos WHERE id = " + idREGISTRO + ";";
+                    int resultado = comando.ExecuteNonQuery();
+                    if (resultado > 0)
+                    {
+                        MessageBox.Show("Contato(s) removido(s) com sucesso! - " + resultado + " registros removidos...");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Contato não encontrado!");
+                    }
+                }
+                catch (Exception erro_mysql)
+                {
+                    // Mensagem de erro - MySQL
+                    // MessageBox.Show(erro_mysql.Message);
+
+                    // Mensagem de erro - USUÁRIO
+                    MessageBox.Show("Erro de Sistema. Solicite ajuda!");
+                }
+                finally
+                {
+                    conexao.Close();
+                }
+                ATUALIZA_Agenda();
+            }
+            else
+            {
+                // MessageBox.Show("NÃO");
+            }
+            LIMPAR_Formulario();
+        }
+
+        private void buttonALTERAR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao.Open();
+
+                comando.CommandText = "UPDATE tbl_contatos SET nome = '" + textBoxNOME.Text + "', sobrenome = '" + textBoxSOBRENOME.Text + "', telefone = '" + textBoxTELEFONE.Text + "', celular = '" + textBoxCELULAR.Text + "', email = '" + textBoxEMAIL.Text + "', rede_social = '" + textBoxREDESOCIAL.Text + "', endereco = '" + textBoxENDERECO.Text + "' WHERE id = " + idREGISTRO + ";";
+                int resultado = comando.ExecuteNonQuery();
+                if (resultado > 0)
+                {
+                    MessageBox.Show("Contato(s) atualizado(s) com sucesso! - " + resultado + " registros atualizados...");
+                }
+                else
+                {
+                    MessageBox.Show("Contato não encontrado!");
+                }
+            }
+            catch (Exception erro_mysql)
+            {
+                // Mensagem de erro - MySQL
+                // MessageBox.Show(erro_mysql.Message);
+
+                // Mensagem de erro - USUÁRIO
+                MessageBox.Show("Erro de Sistema. Solicite ajuda!");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            ATUALIZA_Agenda();
+            LIMPAR_Formulario();
+        }
+
+        private void dataGridViewAGENDA_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            idREGISTRO = dataGridViewAGENDA.CurrentRow.Cells[0].Value.ToString();
+
+            textBoxNOME.Text = dataGridViewAGENDA.CurrentRow.Cells[1].Value.ToString();
+            textBoxSOBRENOME.Text = dataGridViewAGENDA.CurrentRow.Cells[2].Value.ToString();
+            textBoxTELEFONE.Text = dataGridViewAGENDA.CurrentRow.Cells[3].Value.ToString();
+            textBoxCELULAR.Text = dataGridViewAGENDA.CurrentRow.Cells[4].Value.ToString();
+            textBoxEMAIL.Text = dataGridViewAGENDA.CurrentRow.Cells[5].Value.ToString();
+            textBoxREDESOCIAL.Text = dataGridViewAGENDA.CurrentRow.Cells[7].Value.ToString();
+            textBoxENDERECO.Text = dataGridViewAGENDA.CurrentRow.Cells[6].Value.ToString();
+        }
+
+        private void buttonBUSCA_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBoxPESQUISA.Text == "")
+                {
+                    ATUALIZA_Agenda();
+                }
+                else
+                {
+                    conexao.Open();
+
+                    comando.CommandText = "SELECT * FROM tbl_contatos WHERE nome = '" + textBoxPESQUISA.Text + "';";
+                    MySqlDataAdapter adaptadorAGENDA = new MySqlDataAdapter(comando);
+                    DataTable tabelaAGENDA = new DataTable();
+                    adaptadorAGENDA.Fill(tabelaAGENDA);
+
+                    dataGridViewAGENDA.DataSource = tabelaAGENDA;
+                    dataGridViewAGENDA.Columns["id"].HeaderText = "Código";
+                }
+            }
+            catch (Exception erro_mysql)
+            {
+                MessageBox.Show(erro_mysql.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        private void dataGridViewPESQUISA_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
